7a08f08614823ca244623a6cb13106ea6a382c25
26wksk-ctrl <26wksk@gmail.com>
2026-06-16 19:05:29 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -20,11 +20,11 @@ export const TEACHER_EMAILS = ['visionaryshl@gmail.com'];
 // --- 구글 시트 내보내기 (Apps Script 웹앱 브리지) ---
 // Apps Script 를 웹앱으로 배포한 뒤 발급된 URL 을 여기에 붙여넣으세요.
 // 비어 있으면 교사 대시보드의 "구글 시트로 내보내기" 가 동작하지 않습니다.
-export const SHEETS_WEBAPP_URL = '';
+export const SHEETS_WEBAPP_URL = 'https://script.google.com/macros/s/AKfycbyFLSdgs-ZJwtDiJWyV_pqBWITb5bmM5MIMEjgEcXaCwyc2zJP6jR-GAtn8WXIGWaag/exec';
 
 // 시트 엔드포인트 보호용 간단 토큰. (Apps Script 의 EXPORT_TOKEN 과 동일하게 맞추세요)
 // 주의: 클라이언트에 노출되므로 강한 보안은 아니며, 무작위 접근을 막는 최소 방어입니다.
-export const SHEETS_TOKEN = 'change-this-token-1234';
+export const SHEETS_TOKEN = '123412341234';
 
 export const firebaseConfig = {
   apiKey: 'YOUR_API_KEY',
